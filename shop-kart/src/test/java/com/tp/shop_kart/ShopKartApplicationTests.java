6c26c9d87fa49d863315344e5996ae7397b40fbc
thejas <pthejaz@gmail.com>
2025-01-30 17:23:34 +0530

    first commit

---
+++ shop-kart/src/test/java/com/tp/shop_kart/ShopKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.shop_kart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
