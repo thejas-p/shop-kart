21a50d0baedee215a50ed60d5bed6919d81a5ecb
thejas <pthejaz@gmail.com>
2025-02-04 18:57:26 +0530

    cleaned code

--- shop-kart/src/main/java/com/tp/shop_kart/model/Orders.java
+++ shop-kart/src/main/java/com/tp/shop_kart/model/Orders.java
@@ -20,35 +20,4 @@ public class Orders {
     private List<Product> products;
     private double totalPrice;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
-
-    public double getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
 }
