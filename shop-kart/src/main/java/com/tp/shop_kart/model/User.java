2387387b985fc54ee1f8cb238f38088b91fdc8b0
thejas <pthejaz@gmail.com>
2025-02-04 19:04:01 +0530

    cleaned code

--- shop-kart/src/main/java/com/tp/shop_kart/model/User.java
+++ shop-kart/src/main/java/com/tp/shop_kart/model/User.java
@@ -17,27 +17,4 @@ public class User {
     private String username;
     private String email;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
