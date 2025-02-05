3ea9b4c849548fa95a2dc93d1c16b1efcebd4b70
thejas <pthejaz@gmail.com>
2025-02-05 20:56:52 +0530

    added user controller and services

---
+++ shop-kart/src/main/java/com/tp/shop_kart/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.tp.shop_kart.controller;
+
+import com.tp.shop_kart.model.User;
+import com.tp.shop_kart.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+    @PostMapping()
+    public ResponseEntity<?> addUser(@RequestBody User user1){
+        User user=userService.createNewUser(user1);
+        return ResponseEntity.ok(user);
+    }
+    @GetMapping()
+    public ResponseEntity<?> getAllUser(){
+        List<User> users=userService.getAllUser();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserId(@PathVariable long id){
+        User user= userService.getUserById(id);
+        return ResponseEntity.ok(user);
+
+    }
+}
--- shop-kart/src/main/java/com/tp/shop_kart/service/UserService.java
+++ shop-kart/src/main/java/com/tp/shop_kart/service/UserService.java
@@ -1,4 +1,36 @@
 package com.tp.shop_kart.service;
 
+import com.tp.shop_kart.exception.ResourceNotFoundException;
+import com.tp.shop_kart.model.User;
+import com.tp.shop_kart.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User createNewUser(User user){
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUser(){
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id){
+        Optional<User> user=userRepository.findById(id);
+        if(user.isPresent()){
+            return user.get();
+
+        }
+        else {
+            throw new ResourceNotFoundException("User with this id is not found");
+        }
+    }
 }
